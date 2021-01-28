5df9a9f39f1c47a8b75bc9bc43d05116ea72f1e7
Julia Kurganskaia <yulia.kurganskaya@gmail.com>
2021-01-29 11:31:10 +1300

    Rename audio files

--- client/components/Meditation.jsx
+++ client/components/Meditation.jsx
@@ -1,45 +1,25 @@
 //@ts-check
 
 import React from "react";
+import { Link } from "react-router-dom";
+import Mantras from "./Mantras";
+import Sounds from "./Sounds";
 
 const Meditation = () => {
-    const [showMantras, setShowMantras] = React.useState(false);
-    const onClick = () => {
-        if (showMantras === true) {
-            setShowMantras(false);
-        } else {
-            setShowMantras(true);
-        }
-    };
-
     return (
         <div className="meditation-main">
-            <div className="mantras-menu">
-                <button onClick={onClick}>Mantras</button>
+            <div>
+                <div className="home-button">
+                    <Link to={`/`}><button >Home</button></Link>
 
-                {showMantras ? <div>
-                    <div>Meditation for Positive Energy</div>
-                    <audio controls>
-                        <source src="/mantras/1.opus" type="audio/ogg" />
-                        Your browser does not support the audio element.
-                    </audio>
+                </div>
 
-                    <div>10-Minute Meditation For Anxiety</div>
-                    <audio controls>
-                        <source src="/mantras/2.opus" type="audio/ogg" />
-                        Your browser does not support the audio element.
-                    </audio>
-
-                    <div>OM Mantra Meditation</div>
-                    <audio controls>
-                        <source src="/mantras/3.opus" type="audio/ogg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div> : null}
-
+                <Mantras />
+                <Sounds />
             </div>
+
             <img className="spiral" src="/spinning/spinning-spiral.gif" />
-            <div className="sounds-menu">Sounds</div>
+
         </div>
     );
 };
